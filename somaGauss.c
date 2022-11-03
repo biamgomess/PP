6842e19fd75293f6d3aab92afa31f5a2768b66c2
Your Name <you@example.com>
2022-11-03 09:39:48 -0300

    somaGauss

---
+++ somaGauss.c
@@ -0,0 +1,13 @@
+/*
+* Arquivo: aritmInt.c
+* Data de criação: 03.nov.22
+* Beatriz Martins Gomes Silva
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int x;
+    printf("Digite um número: ");
+    scanf("%d")
+}
