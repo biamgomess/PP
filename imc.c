e17d79ca237670b14b29c8bf1a24f8b15730b6c2
Beatruz Martins <biamgomess@outlook.com>
2022-11-03 20:21:28 -0300

    Básico4

--- imc.c
+++ imc.c
@@ -20,29 +20,29 @@ int main(void){
     imc = peso / (altura*altura);
 
     if (imc<16){
-        printf(" %f --> Perigo de vida!", imc);
+        printf("%.2ff --> Perigo de vida!", imc);
     }
     if (imc>16 && imc<17){
-         printf(" %f --> Muito abaixo do peso", imc);
+         printf("%.2f --> Muito abaixo do peso", imc);
     }
     if (imc>17 && imc<18.5){
-        printf(" %f --> Abaixo do peso", imc);  
+        printf("%.2ff --> Abaixo do peso", imc);  
     }
     if (imc>18.5 && imc<25){
-        printf(" %f --> Peso normal", imc);   
+        printf("%.2f --> Peso normal", imc);   
     }
     if(imc>25 && imc<30){
-        printf(" %f -->Acima do peso", imc);
+        printf("%.2f -->Acima do peso", imc);
     }
     if(imc>30 && imc<35){
-        printf(" %f --> Obesidade grau I", imc);
+        printf("%.2f --> Obesidade grau I", imc);
     }
     if(imc>35 && imc<40){
-        printf(" %f --> Obesidade grau II", imc);
+        printf("%.2f --> Obesidade grau II", imc);
 
     }
     if(imc>40){
-        printf(" %f --> Obesidade grau III", imc);
+        printf("%.2f --> Obesidade grau III", imc);
     }
     return 0;
 
