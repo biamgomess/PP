2971d1ac9fadec9e59a60a92e6417c929c4a3d2b
Beatruz Martins <biamgomess@outlook.com>
2022-11-03 21:07:42 -0300

    Médio2

---
+++ fibonacci.c
@@ -0,0 +1,36 @@
+/*
+* Arquivo: aritmInt.c
+* Data de criação: 03.nov.22
+* Beatriz Martins Gomes 
+*/
+
+
+#include <stdio.h>
+
+int main(void) {
+
+    int a;
+    a = 0;
+    int b;
+    b = 1;
+
+    int n;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+    
+    if (n == 1){
+        printf("%d\n", a);
+    } 
+   
+    int i;
+    int auxiliar; 
+    
+    for (i = 3; i <= n; i++) {
+        auxiliar = a + b;
+        a = b;
+        b = auxiliar;
+
+        printf(" %d, ", auxiliar);
+    }
+}
